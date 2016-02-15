e042d8e8d45ccfaaa805e2e207cdbc5fde3e758b
kerazarek <zarek.siegel@gmail.com>
2016-02-15 10:09:36 -0500

    more graphs progress

--- postdocking_graphing/hepi_graphs.R
+++ postdocking_graphing/hepi_graphs.R
@@ -1,22 +1,22 @@
-### Docking data graphs (p300)
+### Docking data graphs (hepi)
 #   2/14/16 Zarek Siegel
 
 rm(list=ls(all=TRUE)) # clear out old variables
 library(foreign) # for reading csv
 
 ### Basic parameters
-dock <- "p31" # docking id
+dock <- "h29" # docking id
 if(substr(dock,0,1) == "p") {prot <- "p300"} # p300 dockings are labeled p##
 if(substr(dock,0,1) == "h") {prot <- "hepi"} # hepi dockings are labeled h##
 # assumptions
 #   -dockings have a docking id of the form d##, 
 #     where d represents the protein and ## is a number
-#   -inside the home docking directory, 
+#   -inallo the home docking directory, 
 #     there is a directory with the protein's name (e.g. .../p300/).
 #     (this is what I mean by 'dock directory')
-#   -inside the protein directory directory, 
+#   -inallo the protein directory directory, 
 #     there is a directory with the docking id (e.g. .../p300/p27/)
-#   -inside the docking directory,
+#   -inallo the docking directory,
 #     threre is a CSV file called d##_alldata.csv (** see zarek or a sample for details)
 
 ### System directories
@@ -33,10 +33,14 @@ data <- read.csv(alldata.csv)
 
 ### Look up ligset
 # ** add this**, this (below) is temporary
-ligset.list <- c("EGCG", "Garcinol", "CTPB", "CTB", "C646")
+ligset.list <- c("adph", "fdla", "ab", "gb", "aam", 
+                 "abm", "gam", "gbm", "ab3", "gb3", 
+                 "ab6", "gb6", "ab7", "gb7", "aa8", 
+                 "ab8", "ga8", "gb8", "gb8y", "aa10", 
+                 "ab10", "ga10", "gb10")
 ### Look up ligset
 # ** add this**, this (below) is temporary
-binding.sites <- c("lys", "coa", "side", "allo1", "allo2") # "coa_adpp", "coa_pant",
+binding.sites <- c("adph", "fdla")#, "allo")
 
 ### Create binds.in.SITE rows (T/F depending on score fraction)
 binding.threshold <- 0.10 # threshold for binding fraction
@@ -85,28 +89,28 @@ for(l in ligset.list) {
 
 ### Graphs!!!!!!!
 setwd(graphs.dir) # needed for pdf names below
-colors <- c("lightskyblue1", "orange", "red3", "khaki1", "mediumslateblue") # gotta make it pretty, you know
+colors <- c("lightskyblue1", "orange")#, "red3") # gotta make it pretty, you know
 
 # Box plot of binding energies (in all sites) by ligand
-pdf("boxplots_bindingenergy_by_lig_ALL.pdf")
+pdf(paste(dock, "boxplots_bindingenergy_by_lig_ALL.pdf", sep="_"), width=14)
 boxplot(E ~ LIG, data=data, main="Binding Energies by Ligand (all binding sites)",
         xlab="Ligand", ylab="Binding energy (kcal/mol)",
         col=c("darkred", "navy"))
 dev.off()
 
 # Strip chart to show energy frequency by ligand
-pdf("stripcharts_bindingenergy_by_lig_ALL.pdf")
-stripchart(E ~ LIG, data=data, method = "stack", offset=1/15,
+pdf(paste(dock, "stripcharts_bindingenergy_by_lig_ALL.pdf", sep="_"), width=14)
+stripchart(E ~ LIG, data=data, method = "stack", offset=1/40,
            vertical=T, col=c("darkred", "navy"),
            main="Binding Affinity Frequencies by Ligand (all binding sites)",
            xlab="Ligand", ylab="Binding energy (kcal/mol)")
 dev.off()
 
 # Density to show energy frequency by ligand
-pdf("binding_energy_density_by_lig.pdf")
+pdf(paste(dock, "binding_energy_density_by_lig.pdf", sep="_"))
 c <- 1
 c.list <- c
-plot(density(data$E), ylim=c(0,1.65), col=c[1],
+plot(density(data$E), ylim=c(0,10), col=c[1],
      main="Density of Dockings versus Binding Energy",
      xlab="Binding energy (kcal/mol)", ylab="Density")
 for(l in ligset.list) {
@@ -118,14 +122,14 @@ legend("topleft", c("ALL", ligset.list), fill=c.list)
 dev.off()
 
 # Bar plot of binding distribution in each binding site per ligand
-graph.in <- data.frame(row.names = ligset.list)
+binding.distrib.plot <- data.frame(row.names = ligset.list)
 for(nbs in paste("Num.", binding.sites, sep="")) {
-  graph.in[, nbs] <- analysis[, nbs]
+  binding.distrib.plot[, nbs] <- analysis[, nbs]
 }
-graph.in <- t(as.matrix(graph.in))
+binding.distrib.plot <- t(as.matrix(binding.distrib.plot))
 
-pdf("binding_distrib_by_lig.pdf")
-binding.distrib.plot <- barplot(graph.in,  main="Binding Distribution by Ligand",
+pdf(paste(dock, "binding_distrib_by_lig.pdf", sep="_"))
+barplot(binding.distrib.plot,  main="Binding Distribution by Ligand",
         xlab="Ligand", ylab="Number of Ligands in Site",
         legend.text = binding.sites,
         col=colors)
@@ -138,50 +142,33 @@ for(abs in paste("AvgE.", binding.sites, sep="")) {
 }
 AvgE.bysite.plot <- t(as.matrix(AvgE.bysite.plot))
 
-pdf("avgE_by_site.pdf")
-barplot(graph.in, main="Average Energy in Each Binding Site",
+pdf(paste(dock, "avgE_by_site.pdf", sep="_"), width=14)
+barplot(AvgE.bysite.plot, main="Average Energy in Each Binding Site",
         xlab="Ligand", ylab="Binding energy (kcal/mol)",
         legend.text = binding.sites, beside = T,
         col=colors)
 dev.off()
 
-###################################################
-
-# Make data subsets for each binding site
-
-# LYS
-pdf("stripcharts_bindingenergy_by_lig_lys.pdf")
-stripchart(data$E[data$binds.in.lys] ~ data$LIG[data$binds.in.lys], method = "stack", offset=1/15,
+# Strip charts for each binding site
+# ADPH
+pdf(paste(dock, "stripcharts_bindingenergy_by_lig_adph.pdf", sep="_"), width=14)
+stripchart(data$E[data$binds.in.adph] ~ data$LIG[data$binds.in.adph], method = "stack", offset=1/20,
            vertical=T, col=c("darkred", "navy"),
-           main="Binding Affinity Frequencies by Ligand (Lysine Binding Site)",
+           main="Binding Affinity Frequencies by Ligand (ADPH Binding Site)",
            xlab="Ligand", ylab="Binding energy (kcal/mol)")
 dev.off()
-# COA
-pdf("stripcharts_bindingenergy_by_lig_coa.pdf")
-stripchart(data$E[data$binds.in.coa] ~ data$LIG[data$binds.in.coa], method = "stack", offset=1/15,
+# FDLA
+pdf(paste(dock, "stripcharts_bindingenergy_by_lig_fdla.pdf", sep="_"), width=14)
+stripchart(data$E[data$binds.in.fdla] ~ data$LIG[data$binds.in.fdla], method = "stack", offset=1/20,
            vertical=T, col=c("darkred", "navy"),
-           main="Binding Affinity Frequencies by Ligand (Acetyl-CoA Binding Site)",
+           main="Binding Affinity Frequencies by Ligand (FDLA Binding Site)",
            xlab="Ligand", ylab="Binding energy (kcal/mol)")
 dev.off()
-# SIDE
-pdf("stripcharts_bindingenergy_by_lig_all.pdf")
-stripchart(data$E[data$binds.in.side] ~ data$LIG[data$binds.in.side], method = "stack", offset=1/15,
+# ALLO
+pdf(paste(dock, "stripcharts_bindingenergy_by_lig_allo.pdf", sep="_"), width=14)
+stripchart(data$E[data$binds.in.allo] ~ data$LIG[data$binds.in.allo], method = "stack", offset=1/20,
            vertical=T, col=c("darkred", "navy"),
-           main="Binding Affinity Frequencies by Ligand (Side Binding Site)",
-           xlab="Ligand", ylab="Binding energy (kcal/mol)")
-dev.off()
-# ALLO1
-pdf("stripcharts_bindingenergy_by_lig_allo1.pdf")
-stripchart(data$E[data$binds.in.allo1] ~ data$LIG[data$binds.in.allo1], method = "stack", offset=1/15,
-           vertical=T, col=c("darkred", "navy"),
-           main="Binding Affinity Frequencies by Ligand (Allo1 Binding Site)",
-           xlab="Ligand", ylab="Binding energy (kcal/mol)")
-dev.off()
-# ALLO2
-pdf("stripcharts_bindingenergy_by_lig_allo2.pdf")
-stripchart(data$E[data$binds.in.allo2] ~ data$LIG[data$binds.in.allo2], method = "stack", offset=1/15,
-           vertical=T, col=c("darkred", "navy"),
-           main="Binding Affinity Frequencies by Ligand (Allo2 Binding Site)",
+           main="Binding Affinity Frequencies by Ligand (Allosteric Binding Site)",
            xlab="Ligand", ylab="Binding energy (kcal/mol)")
 dev.off()
 
@@ -195,6 +182,7 @@ for(bs in binding.sites) {
 distrib.table <- distrib.table[binding.sites]
 distrib.table.csv <- paste(dock, "bs_distribution_table.csv", sep="_")
 write.csv(distrib.table, file=distrib.table.csv)
+
 
 
 ##############################
