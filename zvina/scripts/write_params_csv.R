6fc3c5146fb5197b09083b79d232115c6d276ae8
Zarek Siegel <zarek.siegel@gmail.com>
2016-03-04 20:12:45 -0500

    finalized first version of write_params_csv.R

--- zvina/scripts/write_params_csv.R
+++ zvina/scripts/write_params_csv.R
@@ -3,28 +3,58 @@
 ### Write parameters.csv using information from docks.csv
 # (c) Zarek Siegel
 # v1 2/16/2016
+# v1.1 3/4/2016
 
-# Required input to the script
+### Required input to the script
 dock <- "DOCK"
 scripts_dir <- "/Users/zarek/GitHub/TaylorLab/zvina/scripts"
 
-# Import filesystem constants
-fs_constants_csv <- paste(scripts_dir, "/filesystem_constants.csv", sep="")
-fs_constants <- read.csv(fs_constants_csv, header=TRUE)
-docking_dir <- as.character(fs_constants$address[fs_constants$constant == "docking_dir"])
-ligsets_dir <- as.character(fs_constants$address[fs_constants$constant == "ligsets_dir"])
-docks_csv <- as.character(fs_constants$address[fs_constants$constant == "docks_csv"])
-gridboxes_csv <- as.character(fs_constants$address[fs_constants$constant == "gridboxes_csv"])
+### Import filesystem constants
+fs_constants_csv <- paste0(scripts_dir, "/filesystem_constants.csv")
+fs_constants_df <- read.csv(fs_constants_csv, header=TRUE)
+docking_dir <- as.character(fs_constants_df$address[fs_constants_df$constant == "docking_dir"])
+ligsets_dir <- as.character(fs_constants_df$address[fs_constants_df$constant == "ligsets_dir"])
+docks_csv <- as.character(fs_constants_df$address[fs_constants_df$constant == "docks_csv"])
+gridboxes_csv <- as.character(fs_constants_df$address[fs_constants_df$constant == "gridboxes_csv"])
 
-# Import docking parameters
-docks <- read.csv(docks_csv, header=TRUE)
-prot <- as.character(docks$Protein[docks$Docking.ID == dock])
-ligset <- as.character(docks$Ligset[docks$Docking.ID == dock])
-box <- as.character(docks$Gridbox[docks$Docking.ID == dock])
-exhaust <- as.character(docks$Exhaustiveness[docks$Docking.ID == dock])
-n_models <- as.character(docks$Number.of.Models[docks$Docking.ID == dock])
-n_cpus <- as.character(docks$Number.of.Cpus[docks$Docking.ID == dock])
+### Import docking parameters
+# Docking CSV
+docks_df <- read.csv(docks_csv, header=TRUE)
+# Basics
+prot <- as.character(docks_df$Protein[docks_df$Docking.ID == dock])
+ligset <- as.character(docks_df$Ligset[docks_df$Docking.ID == dock])
+box <- as.character(docks_df$Gridbox[docks_df$Docking.ID == dock])
+exhaust <- as.character(docks_df$Exhaustiveness[docks_df$Docking.ID == dock])
+n_models <- as.character(docks_df$Number.of.Models[docks_df$Docking.ID == dock])
+n_cpus <- as.character(docks_df$Number.of.CPUs[docks_df$Docking.ID == dock])
+  # flex res
+# Grid Box Parameters
+gridboxes_df <- read.csv(gridboxes_csv, header=TRUE)
+box_center_x <- as.character(gridboxes_df$Center.in.x.dimension[gridboxes_df$Gridbox.Name == box])
+box_center_y <- as.character(gridboxes_df$Center.in.y.dimension[gridboxes_df$Gridbox.Name == box])
+box_center_z <- as.character(gridboxes_df$Center.in.z.dimension[gridboxes_df$Gridbox.Name == box])
+box_size_x <- as.character(gridboxes_df$Size.in.x.dimension[gridboxes_df$Gridbox.Name == box])
+box_size_y <- as.character(gridboxes_df$Size.in.y.dimension[gridboxes_df$Gridbox.Name == box])
+box_size_z <- as.character(gridboxes_df$Size.in.z.dimension[gridboxes_df$Gridbox.Name == box])
+# Ligset?
 
-# Import ligset?
+### Create parameters data frame
+parameters_names_list <- c("dock", "prot", "ligset", "box", 
+                "box_center_x", "box_center_y", "box_center_z", 
+                "box_size_x", "box_size_y", "box_size_z", 
+                "exhaust", "n_models", "n_cpus") # flex
+parameters_values_list <- c(dock, prot, ligset, box, 
+                           box_center_x, box_center_y, box_center_z, 
+                           box_size_x, box_size_y, box_size_z, 
+                           exhaust, n_models, n_cpus) # flex
+parameters_df <- data.frame(row.names = parameters_list)
+parameters_df$parameter <- parameters_names_list
+parameters_df$value <- parameters_values_list
 
-# Write params.csv
+### Write parameters CSV
+parameters_csv <- paste0(docking_dir, "/", prot, "/", dock, "_parameters.csv")
+write.csv(parameters_df, parameters_csv, row.names = F)
+noquote(paste0("---> Parameters CSV for docking ", dock, " has been created. It can be found at:"))
+noquote(parameters_csv)
+
+
