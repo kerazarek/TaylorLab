66a7b0022067ab605bb1c9b7d5568fd8b685e8a9
Zarek Siegel <zarek.siegel@gmail.com>
2016-03-05 21:15:21 -0500

    completed first version of separa_vina_results
    (pvr)

--- zvina/scripts/separate_vina_results.sh
+++ zvina/scripts/separate_vina_results.sh
@@ -15,10 +15,30 @@ MGL_py_bin="/Library/MGLTools/latest/bin/pythonsh"
 # Location of process_VinaResult
 pvr_py="$ADT_dir/Utilities24/process_VinaResult.py"
 
-# Import docking parameters
-source $base_dir/scripts/load_parameters.sh h11
-# base_dir="/Users/zarek/GitHub/TaylorLab/zvina/"
+# Retrieve docking parameters
+source $base_dir\scripts/load_parameters.sh h11
+
+# Retrieve ligset list
+ligset_list_txt=$base_dir\ligsets/$ligset/$ligset\_list.txt
+ligset_list=$(for l in $(cat $ligset_list_txt); do echo $l; done)
 
 # Create a directory for processed files
-# mkdir $base_dir/$prot/processed_pdbqts/
-echo $ligset
+mkdir $base_dir$prot/$dock/processed_pdbqts/
+
+# Relevant directories
+result_pdbqts_dir=$base_dir$prot/$dock/result_pdbqts/
+processed_pdbqts_dir=$base_dir$prot/$dock/processed_pdbqts/
+
+# The actual process_VinaResult step
+receptor_pdbqt=$base_dir$prot/$prot.pdbqt
+for lig in $ligset_list;
+do
+	result_pdbqt=$result_pdbqts_dir$dock\_$lig\_results.pdbqt
+	processed_pdbqt_stem=$processed_pdbqts_dir$dock\_$lig\_m
+	$MGL_py_bin $pvr_py -v -r $receptor_pdbqt \
+						   -f $result_pdbqt \
+						   -o $processed_pdbqt_stem
+done
+
+# *** check for results, prot.pdb, params
+# *** check if already pvr'd
