223eb2fc0b7d628627b2186968ded781d97d1254
Zarek Siegel <zarek.siegel@gmail.com>
2016-03-05 20:38:46 -0500

    switching up load_...

--- zvina/scripts/load_parameters.sh
+++ zvina/scripts/load_parameters.sh
@@ -14,25 +14,33 @@ fs_constants_csv="/Users/zarek/GitHub/TaylorLab/zvina/scripts/filesystem_constan
 # Load basic filesystem constants
 # (base_dir, ligsets_dir, scripts_dir, docks_csv, gridboxes_csv)
 fsc_temp=~/fsc_temp_$(date "+%Y%m%d%H%M%S").txt
+# fsc_temp2=~/fsc_temp2_$(date "+%Y%m%d%H%M%S").txt
 # convert from csv, store in temp filesystem_constants.csv
 while read line
 do
-	constant=$(echo $line | sed 's/^\([^,]\{1,\}\),\([^,]\{1,\}\),/\1=\2/')
-	echo "$constant" | sed 's/ //g' >> $fsc_temp
+	constant=$(echo $line | sed 's/^\([^,]\{1,\}\),\([^,]\{1,\}\),/\1=\"\2\"/')
+	echo "$constant" >> $fsc_temp
 done <$fs_constants_csv
+# cat $fsc_temp | sed $'s/\r$//' > $fsc_temp2
+# fsc_temp2=$(cat $fsc_temp)
+# fsc_temp2=$(for x in $(cat $fsc_temp); do echo $x; done)
+# echo $fsc_temp2
+# cat $fsc_temp
 # source the temp file, then remove it
 source $fsc_temp
-rm -f $fsc_temp
+# rm -f $fsc_temp
+
+echo $base_dir/ff
 
 # Source parameters_csvs
-parameters_csvs=$(echo $base_dir | sed 's/.$/parameters_csvs\//')
-cd $parameters_csvs
-par_temp=~/par_temp_$(date "+%Y%m%d%H%M%S").txt
-while read line
-do
-	param=$(echo $line | sed 's/^\([^,]\{1,\}\),\([^,]\{1,\}\)/\1=\2/')
-	echo "$param" | sed 's/ //g' >> $par_temp
-done <$dock\_parameters.csv
-source $par_temp
-rm -f $par_temp
+# parameters_csvs=$(echo $base_dir | sed 's/.$/parameters_csvs\//')
+# cd $parameters_csvs
+# par_temp=~/par_temp_$(date "+%Y%m%d%H%M%S").txt
+# while read line
+# do
+# 	param=$(echo $line | sed 's/^\([^,]\{1,\}\),\([^,]\{1,\}\)/\1=\2/')
+# 	echo "$param" >> $par_temp
+# done <$dock\_parameters.csv
+# source $par_temp
+# rm -f $par_temp
 
--- zvina/scripts/separate_vina_results.sh
+++ zvina/scripts/separate_vina_results.sh
@@ -3,3 +3,21 @@
 ### process_VinaResult and a bit organization
 # (c) Zarek Siegel
 # v1 3/5/16
+
+### Required input
+dock=$1
+base_dir="/Users/zarek/GitHub/TaylorLab/zvina/"
+# AutoDockTools Directory
+ADT_dir="/Library/MGLTools/latest/MGLToolsPckgs/AutoDockTools/"
+# MGLTools Python binary
+MGL_py_bin="/Library/MGLTools/latest/bin/pythonsh"
+
+# Location of process_VinaResult
+pvr_py="$ADT_dir/Utilities24/process_VinaResult.py"
+
+# Import docking parameters
+source $base_dir/scripts/load_parameters.sh h11
+# base_dir="/Users/zarek/GitHub/TaylorLab/zvina/"
+
+# Create a directory for processed files
+mkdir $base_dir/$prot/processed_pdbqts/
