a93c7f994b8ff71b4628c0a2d92ebe56ab87f197
Zarek Siegel <zarek.siegel@gmail.com>
2016-03-05 22:57:38 -0500

    parse_pdbqt.py v1 seems pretty set

--- zvina/scripts/parse_pdbqt.py
+++ zvina/scripts/parse_pdbqt.py
@@ -6,7 +6,39 @@
 
 import re
 
-class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr'd
+### A class for residues and residue atoms
+#		(input is a string of form 'RES123' or 'RES123_A1')
+
+class Residue:
+	def __init__(self, str):
+		# String
+		self.str = str
+		# Atom & Residue String
+		if re.search(r'^[A-Z]+[0-9]+$', self.str):
+			self.atom = None
+			self.res_str = self.str
+		elif re.search(r'^[A-Z]+[0-9]+_.+$', self.str):
+			self.atom = re.sub(r'^[A-Z]+[0-9]+_', '', self.str)
+			self.res_str = re.sub(r'_[A-Z0-9]+$', '', self.str)
+		else: self.atom = None
+		# Residue Index
+		self.resi = re.sub(r'^[A-Z]+|_?[^_]*$', '', self.str)
+		try:
+			self.resi = int(self.resi)
+		except ValueError:
+			self.resi = None
+		# Residue Name
+		self.resn = re.sub(r'[0-9]+_?[^_]*$', '', self.str)
+		# Dictionary of Props
+		self.dic = {'str' : self.str, 'res_str' : self.res_str,
+			'resi' : self.resi, 'resn' : self.resn, 'atom' : self.atom}
+	def __str__(self):
+		return self.str
+
+### A class for molecules, including ones with data from process_VinaResult.py
+#		(input is a .pdb or .pdbqt file address which may or may not be pvr'd)
+
+class Pdb:
 	def get_pdb_coords(self):
 		_coords = []
 		for line in self.pdb_lines:
@@ -32,7 +64,7 @@ class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr
 		_coords = []
 		for line in self.pdb_lines:
 			if re.search('HETATM', line) or (re.search('ATOM', line)):
-				dic = {
+				_dic = {
 					'atomi' : int(line[6:11]),
 					'atomn' : line[12:16].replace(" ", ""),
 					'resn' : line[17:20].replace(" ", ""),
@@ -52,46 +84,46 @@ class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr
 	def mine_pvr_data(self): # mine data from pvrd file
 		contacts = []
 		for line in self.pdb_lines:
-# 			Energy
+			# Binding Energy
 			if re.search('REMARK VINA RESULT: ', line):
 				self.E = re.sub( r'^REMARK VINA RESULT:[ ]+|[ ]+[^ ]+[ ]+[^ ]+$' ,
 					r'' , line.replace('\n', '')) # [23:31].replace(" ", ""))
 				self.E = float(self.E)
-# 			RMSD_LB
+			# RMSD Lower Bound
 			if re.search('REMARK VINA RESULT: ', line):
 				self.rmsd_lb = re.sub( r'^REMARK VINA RESULT:[ ]+[^ ]+[ ]+|[ ]+[^ ]+$' ,
 					r'' , line.replace('\n', ''))
 				self.rmsd_lb = float(self.rmsd_lb)
-# 			RMSD_UB
+			# RMSD Upper Bound
 			if re.search('REMARK VINA RESULT: ', line):
 				self.rmsd_ub = re.sub( r'^REMARK VINA RESULT:[ ]+[^ ]+[ ]+[ ]+[^ ]+' ,
 					r'' , line.replace('\n', ''))
 				self.rmsd_ub = float(self.rmsd_ub)
-# 			Ligand Efficiency
+			# Ligand Efficiency (whatever that means...)
 			if re.search('USER  AD>  ligand efficiency', line):
 				self.pvr_effic = re.sub( r'USER  AD>  ligand efficiency' ,
 					r'' , line.replace('\n', ''))
 				self.pvr_effic = float(self.pvr_effic)
-# 			Model Number
+			# Model Number
 			if re.search(r'USER  AD> .+ of .+ MODELS', line):
 				self.pvr_model = re.sub( r'USER  AD>| of [0-9]+ MODELS' ,
 					r'' , line.replace('\n', ''))
 				self.pvr_model = int(self.pvr_model)
-# 			TorsDOF
+			# Torsional Degrees of Freesom
 			if re.search('REMARK .+ active torsions:', line):
 				self.torsdof = re.sub( r'REMARK|active torsions:' ,
 					r'' , line.replace('\n', ''))
 				self.torsdof = int(self.torsdof)
-# 			Macro_close_ats
+			# Number of Contacts
 			if re.search('USER  AD> macro_close_ats:', line):
 				self.macro_close_ats = re.sub( r'USER  AD> macro_close_ats:' ,
 					r'' , line.replace('\n', ''))
 				self.macro_close_ats = int(self.macro_close_ats)
-# 			Contacts
+			# Contacts
 			if re.search(r'^USER  AD> [^ ]+:[^ ]+:[^ ]+:[^ ,]+$', line):
 				contacts.append(line.replace('\n', '').replace('USER  AD> ', ''))
 
-# 		Contacts Processing
+		# Contacts Processing
 		self.pvr_resis_objs = []
 		self.pvr_resis = []
 		self.pvr_resis_atoms = []
@@ -100,7 +132,6 @@ class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr
 				c).replace(':', '_')))
 
 		for r in self.pvr_resis_objs:
-# 			print(r.dic)
 			if r.atom != None:
 				self.pvr_resis_atoms.append(r.str)
 				self.pvr_resis.append(r.res_str)
@@ -125,9 +156,6 @@ class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr
 		}
 
 	def res_list(self, atoms = False):
-		_op = "res_list"
-# 		announce(_op)
-
 		list = []
 		if atoms is False:
 			for atom in self.coords:
@@ -142,24 +170,20 @@ class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr
 			reslist.append(n)
 		return reslist
 
-
 	def get_type(self):
-		_op = "get_type"
-# 		announce(_op)
-
-# 		Detect if its been through ADT process_VinaResult.py
+		# Detect if its been through ADT process_VinaResult.py
 		self.is_pvrd = False # by default
 		for line in self.pdb_lines:
 			if re.search('REMARK VINA RESULT: ', line):
-				self.pvrd()
+				self.mine_pvr_data()
 				self.is_pvrd = True
 
-# 		Determine file type PDB/PDBQT (they are slightly different)
+		# Determine file type PDB/PDBQT (they are slightly different)
 		if self.pdb_file_in[-5:] == 'pdbqt':
-			self.pdbqt()
+			self.get_pdbqt_coords()
 			self.file_type = 'pdbqt'
 		elif self.pdb_file_in[-3:] == 'pdb':
-			self.pdb()
+			self.get_pdb_coords()
 			self.file_type = 'pdb'
 		else:
 			print("!!! BAD FILETYPE !!!")
@@ -171,10 +195,13 @@ class Pdb:  # input is a .pdb or .pdbqt file address which may or may not be pvr
 			with pdb_file_open as f:
 				self.pdb_lines = f.readlines()
 		except IOError:
+			print("! ! ! IOError")
 			pass
 
-
-		self.get_type()
+def main():
+	infile = "/Users/zarek/GitHub/TaylorLab/zvina/hepi/h11/processed_pdbqts/h11_aa8_m1.pdbqt"
+	thing = Pdb(infile)
+	print(thing.pvr_data)
 
 if __name__ == "__main__": main()
 
